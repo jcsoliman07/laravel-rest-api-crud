1b06dd646d3cb63c29e6ca68467f123eba3435d7
Jomar <jomarsoliman01@gmail.com>
2025-08-12 11:36:57 +0800

    feat: update form request valdiation for Product
    - Update StoreProductRequest and UpdateProductRequest data validation
    - Ensures incoming API request meet required field and format rules

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -11,7 +11,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,6 +23,23 @@ class UpdateProductRequest extends FormRequest
     {
         return [
             //
+            'name'          => 'required|string|max:255',
+            'description'   => 'nullable|string|max:1000',
+            'price'         => 'required|numeric|min:0',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required'         => 'The product name is required.',
+            'name.string'           => 'The product name must be a string.',
+            'name.max'              => 'The product name may not be greater than 255 characters.',
+            'description.string'    => 'The description must be a string.',
+            'description.max'       => 'The description may not be greater than 1000 characters.',
+            'price.required'        => 'The price is required.',
+            'price.numeric'         => 'The price must be a number.',
+            'price.min'             => 'The price must be at least 0.',
         ];
     }
 }
