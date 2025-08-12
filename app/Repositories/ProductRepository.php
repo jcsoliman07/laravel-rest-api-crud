acf21bc559a24e30d4fcb9fd57ad235d4f2bc202
Jomar <jomarsoliman01@gmail.com>
2025-08-12 11:07:28 +0800

    feat: set up repository pattern structure
    - Addedd ProductRepository implementing ProductRepositoryInterface
    - Created Interfaces folder to define repository contracts
    - Created RepositoryServiceProviderto bind interfaces to implementations
    - Established clean architecture for maintable and testable code

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\ProductRepositoryInterface;
+use App\Models\Product;
+
+class ProductRepository implements ProductRepositoryInterface
+{
+    public function getAllProducts()
+    {
+        return Product::all();
+    }
+
+    public function getProductById($id)
+    {
+        return Product::findOrFail($id);
+    }
+
+    public function createProduct(array $data)
+    {
+        return Product::create($data);
+    }
+
+    public function updateProduct($id, array $data)
+    {
+        return Product::whereId($id)->update($data);
+    }   
+
+    public function deleteProduct($id)
+    {
+        return Product::destroy($id);
+    }
+}
+{
+    
+}
